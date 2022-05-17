58c64d0c8cd251215053ea5d34bfa3f54fb62da5
Ratchet7x5 <36789694+Ratchet7x5@users.noreply.github.com>
2022-05-17 16:34:18 +1200

    fix package.json files (and express files)

--- client/src/services/articleservice.js
+++ client/src/services/articleservice.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+/**
+ * Current issue: This file doesn't have many comments to explain what is going on here. 
+ * Code Author: Tarun Ramachandran
+ * Code Reviewer: Maliha
+ */
 const getAllArticles = async () => {
   return axios.get("/api/articles").then((articles) => articles.data);
 };
