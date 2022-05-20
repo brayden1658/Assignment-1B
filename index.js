d02b7d363617a72c56d3a7b89e31d1069ffa7be2
Ratchet7x5 <36789694+Ratchet7x5@users.noreply.github.com>
2022-05-20 18:34:34 +1200

    heroku prep: potential final deployment prep (edit index.js)

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use("/api/articles", articles);
 app.use("/api/practices", practices);
 
 // Server React static build -- comment/uncomment here and there
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 })
