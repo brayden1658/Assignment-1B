7705e52131b0caf53d7d3510e27dcbbeb4503533
Ratchet7x5 <36789694+Ratchet7x5@users.noreply.github.com>
2022-05-20 22:46:13 +1200

    bugfixes: 404 (on npm start) console log on App.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import {getAllArticles} from "../services/articleservice";
 
 class Home extends Component {
   render() {
@@ -7,7 +6,6 @@ class Home extends Component {
       <div>
         <h2>Welcome to the SPEED Application!</h2>
         <p>This is the Home Page of the SPEED Application. This is a work in progress!</p>
-        <p>{console.log(JSON.stringify(getAllArticles))}</p>
       </div>
     );
   }
--- client/src/services/articleservice.js
+++ client/src/services/articleservice.js
@@ -1,13 +1,5 @@
 import axios from "axios";
 
-/**
- * Current issue: This file doesn't have many comments to explain what is going on here. 
- * This file also only contains skeleton code, as these functions must be used in other
- * files. 
- * Code Author: Tarun Ramachandran
- * Code Reviewer: Maliha
- */
-
 //gets all articles from the MongoDB
 const getAllArticles = async () => {
   return axios.get("/api/articles").then((articles) => articles.data);
@@ -17,5 +9,7 @@ const postArticle = async (article) => {
   //return axios.post("/api/articles", article).then((articles) => articles.data);
   return axios.post("/api/articles", article);
 }
+
+//todo(ratchet7x5): add in PUT and DELETE here for mods 
 
 export { getAllArticles, postArticle };
